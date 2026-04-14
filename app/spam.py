4b2edbebb63755854589624a69c34ae9dafa48cd
Wonyoung Jung <2655362@naver.com>
2026-04-14 13:51:15 +0900

    fix 에러발생

--- app/spam.py
+++ app/spam.py
@@ -1,7 +1,7 @@
 def check_spam(text: str) -> str:
   text = text.lower().strip()
   if text == "":
-    return "ham"
+    return "ham", 0
   
   spam_keywords = [
     "free", "win", "winner", "prize", "click",
