72e72e2b01504c6a74fc5ddce7ad4b1c04e24726
Wonyoung Jung <2655362@naver.com>
2026-05-14 14:55:33 +0900

    connect mlflow

--- app/config.py
+++ app/config.py
@@ -1,3 +1,6 @@
 import os
 MODEL_MODE = "ml" # "ml"
-LOCAL_MODEL_PATH = "ml/artifacts/spam_model.joblib"
+# LOCAL_MODEL_PATH = "ml/artifacts/spam_model.joblib"
+# MLFLOW_TRACKING_URI = "sqlite:///mlflow.db"
+# MODEL_URI = "runs:/9b3502ed73a244778adfb84ec345b0d7/model"
+MODEL_URI = "models:/spam-model@champion"
